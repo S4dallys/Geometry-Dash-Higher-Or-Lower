9455263251e7c217f3a3e52a0179e055779d4700
S4dallys <geometrygaming8@gmail.com>
2024-05-27 02:01:03 +0800

    changed vh to svh, please work

--- src/lib/components/Overlay.svelte
+++ src/lib/components/Overlay.svelte
@@ -12,7 +12,9 @@
     <Header />
 </div>
 <div
-    class="absolute z-10 w-10 md:w-16 aspect-square leading-[2.5rem] md:leading-[4rem] rounded-full bg-white font-bold text-black align-middle text-center text-xl md:text-2xl top-[50vh] left-1/2 translate-x-[-50%] translate-y-[-50%]"
+    class="absolute z-10 w-10 md:w-16 aspect-square leading-[2.5rem] md:leading-[4rem]
+    rounded-full bg-white font-bold text-black align-middle text-center
+    text-xl md:text-2xl top-[50svh] left-1/2 translate-x-[-50%] translate-y-[-50%]"
 >
     <p class="inline-block">VS</p>
 </div>
