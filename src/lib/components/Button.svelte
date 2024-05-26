f9bbe9e127b91faeb4d342d8b4c47f00db57ae54
S4dallys <geometrygaming8@gmail.com>
2024-05-27 01:56:29 +0800

    vs is not centered??

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <button
-    class="border-white border-2 mx-auto w-[200px] text-yellow-200 hover:bg-white hover:text-black transition font-semibold px-10 py-5 text-sm md:text-lg h-10 rounded-full flex gap-3 items-center"
+    class="border-white border-2 mx-auto w-[200px] text-yellow-200 hover:bg-white hover:text-black transition font-semibold px-10 md:py-5 text-sm md:text-lg h-8 md:h-10 rounded-full flex gap-3 items-center"
     on:click={handleClick}
 >
     <p class="w-20">{value}</p>
