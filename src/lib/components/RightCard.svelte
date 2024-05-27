7bfa120f1495c0421517be63b82dc2ac0bbe36fe
S4dallys <geometrygaming8@gmail.com>
2024-05-27 12:29:58 +0800

    values now break when word is too long

--- src/lib/components/RightCard.svelte
+++ src/lib/components/RightCard.svelte
@@ -10,7 +10,7 @@
     export let chooseHarder;
 </script>
 
-<div class="text-center text-white h-36 md:h-80 w-svw md:w-[50svw]">
+<div class="text-center break-words text-white h-36 md:h-80 w-svw md:w-[50svw]">
     <p class="font-bold text-2xl md:text-5xl md:mb-2">"{name}"</p>
     <p class="italic text-xl md:text-2xl md:mb-5">{author}</p>
     <p class="text-lg my-2 md:mb-10">is</p>
